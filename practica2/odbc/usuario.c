6621ae373ebbfefb809cdc587456d56d3432311d
OscarGB <oscar.gomez.borz@gmail.com>
2016-11-11 14:11:35 +0100

    En proceso

--- practica2/odbc/usuario.c
+++ practica2/odbc/usuario.c
@@ -5,13 +5,12 @@
 #include <sqlext.h>
 #include "odbc.h"
 
-/*
- * example 4 with a queries build on-the-fly, the good way
- */
-
-long mk_new_usrid(SQLDBC dbc) {
-  long id;
+/*Gives back the next available id*/
+int mk_new_usrid(SQLHDBC dbc) {
+  int id;
   char consulta[1000] = "select max(user_id) from fiduser";
+	SQLHSTMT stmt;
+	SQLRETURN ret;
 
   SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
   SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
@@ -27,122 +26,134 @@ long mk_new_usrid(SQLDBC dbc) {
   }
 }
 
+void add_ccard(SQLHDBC dbc, char* ccardno, char* exp){
+	char consulta[1000] = "select max(user_id) from fiduser";
+	SQLHSTMT stmt;
+	
+	sprintf(consulta, "Insert into ccard values (%s, %s)", exp, ccardno); 
 
-int exists_usr(SQLDBC dbc) {
+  SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
+  SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
+	
+	SQLExecute(stmt);
+
+	SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+	return;
 }
 
-
-
-int main(int argc, char *argv[]) {
-    SQLHENV env;
-    SQLHDBC dbc;
-    SQLHSTMT stmt;
-    SQLRETURN ret; /* ODBC API return status */
-    SQLINTEGER x;
-    /*	char fullname[512];
-	char scrname[512]; */
-	unsigned int i, j;
-	char consulta[1000] = "";
-
-	/*	if(!(argc == 2)){
-		printf("Please use the format:\n./usuario +<scrname>\"<fullname>\"\nor\n./usuario -<scrname>\n");
-		return EXIT_SUCCESS;
-	}
-	*/
-
-    /* CONNECT */
-    ret = odbc_connect(&env, &dbc);
-    if (!SQL_SUCCEEDED(ret)) {
-	printf("Error connecting.\n");
-        return EXIT_FAILURE;
-    }
-
-    /* Allocate a statement handle */
-    SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
-
-	if(argv[1][0] == '+'){ /*Starts with + (add user)*/
-	  /*
-		for (i = 0; i < strlen(argv[1]) && argv[1][i + 1] != '\0' && argv[1][i + 1] != '"'; i++){
-			scrname[i] = argv[1][i + 1];			
-		}
-		for(j = i + 2; j < strlen(argv[1]) && argv[1][j] != '\n' && argv[1][j] != '"'; j++){
-			fullname[j - (i + 2)] = argv[1][j];
-		} 
-	  */		
-		strcpy(consulta, "SELECT scrname FROM fiduser WHERE scrname = '");
-		strcat(consulta, argv[2]);
-		strcat(consulta, "';");
-
-		SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
-		
-		SQLExecute(stmt);
-		if(SQL_SUCCEEDED(ret = SQLFetch(stmt))) {
-		  printf("El usuario %s existe\n", argv[2]);
-		}
-
-	}
-		/*
-		
-
-		SQLBindCol(stmt, 1, SQL_INTEGER, &x, sizeof(x), NULL);
-		
-		if(x >= 1){
-			printf("The user does already exists.\n");
-			goto EXIT;
-		}
-
-		strcpy(consulta, "INSERT INTO fiduser (scrname, fullname, ) VALUES(");
-		strcat(consulta, scrname);
-		strcat(consulta, ", "),
-		strcat(consulta, fullname);
-		strcat(consulta, ");");
-		
-		SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
-		
-		SQLExecute(stmt);
-
-		printf("El usuario se ha añadido a la base de datos.\n");
-
-		goto EXIT;		
-	}
-
-	for (i = 0; i < strlen(argv[1]) && argv[1][i + 1] != '\0' && argv[1][i + 1] != '"'; i++){
-		scrname[i] = argv[1][i + 1];			
-	}
+void add_usr(SQLHDBC dbc, char* scrname, char* fullname, char* joindate, char* ccardno){
+	char consulta[1000] = "select max(user_id) from fiduser";
+	SQLHSTMT stmt;
 	
-	strcpy(consulta, "SELECT COUNT(*) FROM fiduser WHERE scrname = '");
-	strcat(consulta, scrname);
-	strcat(consulta, "';");
+	sprintf(consulta, "Insert into fiduser values (%d, %s, %s, %s, FALSE, %s)", mk_new_usrid(dbc), scrname, fullname, joindate, ccardno); 
+
+  SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
+  SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
+	
+	SQLExecute(stmt);
+
+	SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+	return;
+}
+
+int exists_usr(SQLHDBC dbc, char* name) {
+	char consulta[1000];
+	SQLHSTMT stmt;
+	SQLRETURN ret;
+
+	SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
+	sprintf(consulta, "SELECT scrname FROM fiduser WHERE scrname = '%s';", name);
 
 	SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
 	
 	SQLExecute(stmt);
-	SQLBindCol(stmt, 1, SQL_INTEGER, &x, sizeof(x), NULL);
+	if(SQL_SUCCEEDED(ret = SQLFetch(stmt))) {
+	  printf("El usuario %s existe\n", name);
+		SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+		return 0;
+	}
+	else{
+		SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+		return 1;
+	}
+}
+
+int exists_ccard(SQLHDBC dbc, char* ccardno) {
+	char consulta[1000];
+	SQLHSTMT stmt;
+	SQLRETURN ret;
+
+	SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
+	sprintf(consulta, "SELECT ccardno FROM ccard WHERE ccardno = '%s';", ccardno);
+
+	SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
 	
-	if(x == 0){
-		printf("The user does not exist.\n");
-		goto EXIT;
+	SQLExecute(stmt);
+	if(SQL_SUCCEEDED(ret = SQLFetch(stmt))) {
+	  printf("La tarjeta %s existe\n", ccardno);
+		SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+		return 0;
+	}
+	else{
+		printf("La tarjeta %s no existe\n", ccardno);
+		SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+		return 1;
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+  SQLHENV env;
+  SQLHDBC dbc;
+  SQLRETURN ret; /* ODBC API return status */
+ 
+	if(argc < 3){
+		printf("Please use the format:\n./usuario + <scrname> \"<fullname>\" <joinig date(YYYY-MM-DD)> <ccard number> <ccard expiration(YYYY-MM-DD)>\nor\n./usuario - <scrname>\n");
+		return EXIT_FAILURE;
 	}
 
-	strcpy(consulta, "UPDATE fiduser SET borrado = True WHERE scrname = '");
-	strcat(consulta, scrname);
-	strcat(consulta, "';");
+  /* CONNECT */
+  ret = odbc_connect(&env, &dbc);
+  if (!SQL_SUCCEEDED(ret)) {
+		printf("Error connecting.\n");
+    return EXIT_FAILURE;
+  }
 
-	SQLExecute(stmt);
+	if(strcmp(argv[1], "+") == 0){
+		if(exists_usr(dbc, argv[2]) == 0){
+			ret = odbc_disconnect(env, dbc);
+    	if (!SQL_SUCCEEDED(ret)) {
+				printf("Error disconnecting.\n");
+      	return EXIT_FAILURE;
+    	}
+			return EXIT_SUCCESS;
+		}
+		
+		if(exists_ccard(dbc, argv[5]) == 0){
+			ret = odbc_disconnect(env, dbc);
+    	if (!SQL_SUCCEEDED(ret)) {
+				printf("Error disconnecting.\n");
+      	return EXIT_FAILURE;
+    	}
+    	return EXIT_SUCCESS;
+		}
 
-	printf("El usuario se ha borrado de la base de datos.\n");
+		add_ccard(dbc, argv[5], argv[6]);
+		add_usr(dbc, argv[2], argv[3], argv[4], argv[5]);
 
-	EXIT:
-		*/	
-    /* free up statement handle */
-    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
 
-    /* DISCONNECT */
-    ret = odbc_disconnect(env, dbc);
-    if (!SQL_SUCCEEDED(ret)) {
-	printf("Error disconnecting.\n");
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+		/* DISCONNECT */
+		ret = odbc_disconnect(env, dbc);
+		if (!SQL_SUCCEEDED(ret)) {
+			printf("Error disconnecting.\n");
+		  return EXIT_FAILURE;
+		}
+	
+		return EXIT_SUCCESS;
+		
+		
+	}
+	return EXIT_SUCCESS;
 }
+
+		
