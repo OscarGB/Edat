3d3418d0e6efcf130594ff5932a34f29cc3a297a
OscarGB <oscar.gomez.borz@gmail.com>
2016-11-11 13:01:10 +0100

    SANTINI

--- practica2/odbc/usuario.c
+++ practica2/odbc/usuario.c
@@ -9,22 +9,46 @@
  * example 4 with a queries build on-the-fly, the good way
  */
 
+long mk_new_usrid(SQLDBC dbc) {
+  long id;
+  char consulta[1000] = "select max(user_id) from fiduser";
+
+  SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
+  SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
+  SQLBindCol(stmt, 1, SQL_INTEGER, &id, sizeof(id), NULL);  
+  SQLExecute(stmt);
+  if(SQL_SUCCEEDED(ret = SQLFetch(stmt))) {
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+    return id+1;
+  } 
+  else {
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+    return 1;
+  }
+}
+
+
+int exists_usr(SQLDBC dbc) {
+}
+
+
+
 int main(int argc, char *argv[]) {
     SQLHENV env;
     SQLHDBC dbc;
     SQLHSTMT stmt;
     SQLRETURN ret; /* ODBC API return status */
     SQLINTEGER x;
-	char fullname[512];
-	char scrname[512];
+    /*	char fullname[512];
+	char scrname[512]; */
 	unsigned int i, j;
 	char consulta[1000] = "";
 
-	if(!(argc == 2)){
+	/*	if(!(argc == 2)){
 		printf("Please use the format:\n./usuario +<scrname>\"<fullname>\"\nor\n./usuario -<scrname>\n");
 		return EXIT_SUCCESS;
 	}
-	
+	*/
 
     /* CONNECT */
     ret = odbc_connect(&env, &dbc);
@@ -37,20 +61,29 @@ int main(int argc, char *argv[]) {
     SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
 
 	if(argv[1][0] == '+'){ /*Starts with + (add user)*/
+	  /*
 		for (i = 0; i < strlen(argv[1]) && argv[1][i + 1] != '\0' && argv[1][i + 1] != '"'; i++){
 			scrname[i] = argv[1][i + 1];			
 		}
 		for(j = i + 2; j < strlen(argv[1]) && argv[1][j] != '\n' && argv[1][j] != '"'; j++){
 			fullname[j - (i + 2)] = argv[1][j];
 		} 
-		
-		strcpy(consulta, "SELECT COUNT(*) FROM fiduser WHERE scrname = '");
-		strcat(consulta, scrname);
+	  */		
+		strcpy(consulta, "SELECT scrname FROM fiduser WHERE scrname = '");
+		strcat(consulta, argv[2]);
 		strcat(consulta, "';");
 
 		SQLPrepare(stmt, (SQLCHAR*) consulta, SQL_NTS);
 		
 		SQLExecute(stmt);
+		if(SQL_SUCCEEDED(ret = SQLFetch(stmt))) {
+		  printf("El usuario %s existe\n", argv[2]);
+		}
+
+	}
+		/*
+		
+
 		SQLBindCol(stmt, 1, SQL_INTEGER, &x, sizeof(x), NULL);
 		
 		if(x >= 1){
@@ -100,7 +133,7 @@ int main(int argc, char *argv[]) {
 	printf("El usuario se ha borrado de la base de datos.\n");
 
 	EXIT:
-	
+		*/	
     /* free up statement handle */
     SQLFreeHandle(SQL_HANDLE_STMT, stmt);
 
