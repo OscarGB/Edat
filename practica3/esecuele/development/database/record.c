9d79222cc5041f6da3a04a1027ed05a79d53ca35
nagomez97 <nagomez97@gmail.com>
2016-11-30 19:50:19 +0100

    records nacho

--- practica3/esecuele/development/database/record.c
+++ practica3/esecuele/development/database/record.c
@@ -1,21 +1,49 @@
 #include "record.h"
 
 struct record_ {
-  /* To be implemented */
+  int ncols;
+  long next;
+  void** values;
+
 };
 
 record_t* record_create(void** values, int ncols, long next) {
-  /* To be implemented */
+  record_t* record = NULL;
+
+  if(!values || ncols <= 0 || next <= 0) return NULL:
+
+  record = (record_t*)malloc(sizeof(record_t));
+  if(!record) return NULL;
+
+  record->ncols = ncols;
+
+  record->next = next;
+
+  record->values = values;
+
+  return record;
 }
 
 void* record_get(record_t* record, int n) {
-  /* To be implemented */
+  if(!record || n < 0 || n >= record->ncols) return NULL;
+
+  return record->values[n];
 }
 
 long record_next(record_t* record) {
-  /* To be implemented */
+  if(!record) return -1;
+
+  return record->next;
 }
 
 void record_free(record_t* record) {
-  /* To be implemented */
+  int i;
+
+  for(i = 0; i < record->ncols; i++){
+  	if(record->values[i] != NULL){
+  		free(record->values[i]);
+  	}
+  }
+
+  free(record);
 }
